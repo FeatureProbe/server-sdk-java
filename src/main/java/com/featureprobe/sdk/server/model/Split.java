7f11e8883376ab856f73eec53a1ec52c44d5a9a5
jianggang <jianggang@didiglobal.com>
2022-08-25 12:10:09 +0800

    feat: add stable rollout function for FPUser

--- src/main/java/com/featureprobe/sdk/server/model/Split.java
+++ src/main/java/com/featureprobe/sdk/server/model/Split.java
@@ -35,7 +35,7 @@ public class Split {
     }
 
     public HitResult findIndex(FPUser user, String toggleKey) {
-        String hashKey = StringUtils.isNoneEmpty(user.getKey()) ? user.getKey() : String.valueOf(System.nanoTime());
+        String hashKey = user.getKey() != null ? user.getKey() : String.valueOf(System.nanoTime());
         if (StringUtils.isNotBlank(bucketBy)) {
             if (user.containAttr(bucketBy)) {
                 hashKey = user.getAttr(bucketBy);
